e98e60e658d807f0aa6c07f308f19d6d5617ab64
klong <karl_longman@hotmail.com>
2017-02-01 22:20:45 +0000

    extra docs

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -2,6 +2,15 @@ defmodule Issues.TableFormatter do
     
     import Enum, only: [each: 2, map: 2, map_join: 3, max: 1]
 
+    @doc """
+    Takes a list of row data, where each row is a Map, and a list of
+    headers. Prints a table to STDOUT of the data from each row 
+    identified by each header. That is, each header identifies a column,
+    and those columns are extracted and printed from the rows.
+    We calculate the width of each column to fit the longest element 
+    in that column.
+    """
+
     def print_table_for_columns(rows, headers) do
         with data_by_columns = split_into_columns(rows, headers),
             column_widths = widths_of(data_by_columns),
@@ -13,26 +22,54 @@ defmodule Issues.TableFormatter do
         end
     end
 
+    @doc """
+    Given a list of rows, where each row contains a keyed list 
+    of columns, return a list containing lists of the data in each column.
+    The 'headers' parameter contains the list of columns to extract 
+
+    ## Example        
+        iex> list = [Enum.into([{"a", "1"},{"b", "2"},{"c", "3"}], %{}),​
+               ...>         Enum.into([{"a", "4"},{"b", "5"},{"c", "6"}], %{})]​
+​ 	​      iex> Issues.TableFormatter.split_into_columns(list, [ "a", "b", "c" ])​
+​ 	​      [ ["1", "4"], ["2", "5"], ["3", "6"] ]​
+    """
+
     def split_into_columns(rows,headers) do
         for header <- headers do
             for row <- rows, do: printable(row[header])
         end
     end
 
+
+
     def printable(str) when is_binary(str), do: str 
     def printable(str), do: to_string(str)
+
+    @doc """
+​ 	​  Given a list containing sublists, where each sublist contains the data for​
+​ 	​  a column, return a list containing the maximum width of each column​
+​ 	
+​ 	​  ## Example​
+​ 	​      iex> data = [ [ "cat", "wombat", "elk"], ["mongoose", "ant", "gnu"]]​
+​ 	​      iex> Issues.TableFormatter.widths_of(data)​
+​ 	​      [ 6, 8 ]​​ 	​  
+    """
 
     def widths_of(columns) do
         for column <- columns, do: column |> map(&String.length/1) |> max
     end
 
+
    def format_for(column_widths) do
     map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
   end
 
+
+
    def separator(column_widths) do
     map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
   end
+  
 
     def puts_in_columns(data_by_columns, format) do
         data_by_columns
